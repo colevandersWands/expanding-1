285c0f912f8a2893fb794ae7cf6d249290cc2f10
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-12-19 19:54:08 +0100

    template-

---
+++ example.js
@@ -0,0 +1,15 @@
+console.log('--- og code ---')
+
+  
+  for (let i = 0; i < 10; i++) {
+    console.log(i)
+  }
+
+
+console.log('--- expanded code ---')
+
+  let i = 0;
+  while (i < 10) {
+    console.log(i)
+    i++;
+  }
---
+++ template.js
@@ -0,0 +1,10 @@
+console.log('--- og code ---')
+
+  // some basic snippet
+  // minimal variables
+  // console.logs at key points
+
+console.log('--- expanded code ---')
+
+  // expand the snippet above
+  // logs should be the same
